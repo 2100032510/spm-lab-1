1159a33d7fbeaaeb68c28a8aef96a4566f761a48
2100032510 <2100032510@kluniversity.in>
2023-12-12 10:17:40 +0530

    changed

--- fact.java
+++ fact.java
@@ -11,19 +11,4 @@ public class Factorial {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a non-negative integer: ");
-        int num = scanner.nextInt();
-
-        if (num < 0) {
-            System.out.println("Please enter a non-negative integer.");
-        } else {
-            int factorial = calculateFactorial(num);
-            System.out.println("The factorial of " + num + " is: " + factorial);
-        }
-
-        scanner.close();
-    }
-}
+    my name Dr. abdulrahman
